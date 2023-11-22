c2906af0ac8a7bc624f33b5c9263bca26e5bb214
Ivan <jonnykold@mail.ru>
2023-11-22 21:22:55 +0100

    Initiate DB

--- app/configs.py
+++ app/configs.py
@@ -1,3 +1,5 @@
+from urllib.parse import ParseResult, urlparse
+
 from json import loads
 from os import environ
 from typing import Any, Dict, List
@@ -5,9 +7,9 @@ from typing import Any, Dict, List
 from pydantic import BaseModel
 
 
-def parse(cast: Any, setting: str, separator: str) -> Any:
+def parse(cast: Any, setting: str, separator: str, **extra) -> Any:
     if cast is str:
-        return setting
+        return _parse_str(setting, **extra)
     elif cast is bool:
         return _parse_bool(setting)
     elif cast in (list, List):
@@ -15,6 +17,29 @@ def parse(cast: Any, setting: str, separator: str) -> Any:
     elif cast in (dict, Dict):
         return _parse_dict(setting)
     raise NotImplementedError(f'Cast is not implemented. cast="{cast}"') from None
+
+
+def _parse_str(setting: str, **extra):
+    try:
+        is_db = extra["db_url"]
+    except KeyError:
+        return setting
+    if is_db:
+        return _fix_db_url(setting, **extra)
+    return setting
+
+
+def _fix_db_url(url: str, **extra):
+    db_url = urlparse(url)
+    if db_url.scheme.split("+")[0] in ["postgres", "postgresql"]:
+        return _fix_postgres_url(db_url, extra.get("async_", False))
+    raise NotImplementedError() from None
+
+
+def _fix_postgres_url(url: ParseResult, is_async=False):
+    if is_async:
+        return url._replace(scheme="postgresql+asyncpg").geturl()
+    return url._replace(scheme="postgresql").geturl()
 
 
 def _parse_list(setting: str, separator: str) -> List:
@@ -55,6 +80,7 @@ def get_env_var(
     separator: str = ",",
     required=True,
     default: Any = None,
+    **extra,
 ) -> Any:
     try:
         setting = environ[var]
@@ -62,11 +88,12 @@ def get_env_var(
         if not required:
             return default
         raise RuntimeError(f'Missing environment variable. variable="{var}"') from None
-    return parse(cast, setting, separator)
+    return parse(cast, setting, separator, **extra)
 
 
 class Settings(BaseModel):
     allow_origins: List = get_env_var("ALLOW_ORIGINS", cast=List)
+    database_url: List = get_env_var("DATABASE_URL", cast=str, async_=True, db_url=True)
 
 
 settings = Settings()
---
+++ app/repositories/sessions.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from app.configs import settings
+
+async_engine = create_async_engine(
+    settings.database_url,
+    echo=True,
+    future=True,
+)
+
+
+async def get_async_session() -> AsyncSession:
+    async_session = async_sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+    async with async_session() as session:
+        yield session
