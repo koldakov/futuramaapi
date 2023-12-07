ba7af17b5b3f05284d117119572bf83fc658d536
Ivan Koldakov <ivan@koldakov.com>
2023-12-07 19:45:14 +0100

    Use relative paths in templates

--- app/templates.py
+++ app/templates.py
@@ -1,6 +1,9 @@
 import gettext
 
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
+from jinja2 import pass_context
+from starlette.datastructures import URL
 
 try:
     gnu_translations = gettext.translation(
@@ -12,5 +15,13 @@ except FileNotFoundError:
     raise RuntimeError("Please compile messages first")
 
 
+@pass_context
+def relative_path_for(context: dict, name: str, /, **path_params) -> str:
+    request: Request = context["request"]
+    http_url: URL = request.url_for(name, **path_params)
+    return http_url.path
+
+
 templates = Jinja2Templates(directory="templates", extensions=["jinja2.ext.i18n"])
+templates.env.globals["relative_path_for"] = relative_path_for
 templates.env.install_gettext_translations(gnu_translations)
