e91ab3f308cd8e079321c7d4893892fa90d3e582
Ivan Koldakov <ivan@koldakov.com>
2023-12-25 22:55:53 +0100

    Add to dict method to all models

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,3 +1,22 @@
+from typing import List
+
 from sqlalchemy.orm import declarative_base
 
-Base = declarative_base()
+_Base = declarative_base()
+
+
+class Base(_Base):
+    __abstract__ = True
+
+    def to_dict(
+        self,
+        *,
+        exclude: List = None,
+    ) -> dict:
+        if exclude is None:
+            exclude = []
+        return {
+            k: v
+            for k, v in self.__dict__.items()
+            if not str(k).startswith("_") and str(k) not in exclude
+        }
