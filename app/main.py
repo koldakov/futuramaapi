23d87817c7cd3aee7cda5987a40bbc7b39c8ce1c
Ivan Koldakov <ivan@koldakov.com>
2023-12-26 22:09:51 +0100

    Remove allow credentials
    Allow creadentials can't be used with "*" as
    origins. That's a public API, so we don't need
    any auth, at least for now.

--- app/main.py
+++ app/main.py
@@ -24,7 +24,6 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.allow_origins,
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
