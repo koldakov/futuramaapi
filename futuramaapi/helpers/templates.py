5283a41d726a5242a7c7ca6bdbfd524ba8e1552c
Ivan Koldakov <coldie322@gmail.com>
2024-07-16 21:23:26 +0700

    Return the real relative url

--- futuramaapi/helpers/templates.py
+++ futuramaapi/helpers/templates.py
@@ -3,19 +3,19 @@ from typing import TYPE_CHECKING
 
 from fastapi.templating import Jinja2Templates
 from jinja2 import pass_context
+from starlette.datastructures import URL
 
 if TYPE_CHECKING:
     from fastapi import Request
-    from starlette.datastructures import URL
 
 TEMPLATES_PATH: Path = Path("templates")
 
 
 @pass_context
-def relative_path_for(context: dict, name: str, /, **path_params) -> str:
+def relative_path_for(context: dict, name: str, /, **path_params) -> URL:
     request: "Request" = context["request"]
     http_url: "URL" = request.url_for(name, **path_params)
-    return http_url.path
+    return http_url.replace(netloc="", scheme="")
 
 
 def _build_templates(templates_dir: Path) -> Jinja2Templates:
