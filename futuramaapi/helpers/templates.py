b27ad81323cd46a5d98866bae52894c48531af2e
Ivan Koldakov <coldie322@gmail.com>
2025-03-14 20:30:27 +0200

    Make templates more flexible

--- futuramaapi/helpers/templates.py
+++ futuramaapi/helpers/templates.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
-from fastapi.templating import Jinja2Templates
-from jinja2 import pass_context
+from fastapi.templating import Jinja2Templates as _Jinja2Templates
+from jinja2 import Environment, pass_context
 from starlette.datastructures import URL
 
 if TYPE_CHECKING:
@@ -11,17 +11,32 @@ if TYPE_CHECKING:
 TEMPLATES_PATH: Path = Path("templates")
 
 
-@pass_context
-def relative_path_for(context: dict, name: str, /, **path_params) -> URL:
-    request: Request = context["request"]
-    http_url: URL = request.url_for(name, **path_params)
-    return http_url.replace(netloc="", scheme="")
+class Jinja2Templates(_Jinja2Templates):
+    def _setup_env_defaults(self, env: Environment) -> None:
+        @pass_context
+        def url_for(
+            context: dict[str, Any],
+            name: str,
+            /,
+            **path_params: Any,
+        ) -> URL:
+            request: Request = context["request"]
+            return request.url_for(name, **path_params)
+
+        env.globals.setdefault("url_for", url_for)
+
+        @pass_context
+        def relative_path_for(
+            context: dict[str, Any],
+            name: str,
+            /,
+            **path_params: Any,
+        ) -> URL:
+            request: Request = context["request"]
+            http_url: URL = request.url_for(name, **path_params)
+            return http_url.replace(netloc="", scheme="")
+
+        env.globals.setdefault("relative_path_for", relative_path_for)
 
 
-def _build_templates(templates_dir: Path) -> Jinja2Templates:
-    _templates: Jinja2Templates = Jinja2Templates(directory=str(templates_dir))
-    _templates.env.globals["relative_path_for"] = relative_path_for
-    return _templates
-
-
-templates: Jinja2Templates = _build_templates(TEMPLATES_PATH)
+templates: Jinja2Templates = Jinja2Templates(TEMPLATES_PATH)
