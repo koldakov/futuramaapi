523327074872461f79ead34f80f7f11b9c8a7034
Ivan Koldakov <ivan.koldakov@gmail.com>
2025-08-02 21:44:09 +0300

    Add proper test ENV

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,7 @@
-import os
+import pytest
+from dotenv import load_dotenv
 
 
-def from_file(
-    filename: str,
-    /,
-    *,
-    separator: str = "=",
-    symbol_comment: str = "#",
-) -> None:
-    with open(filename) as file:
-        data = file.read()
-
-    for line in data.splitlines():
-        if not line.strip() or line.startswith(symbol_comment):
-            continue
-
-        key, value = line.split()[0].split(separator, 1)
-        os.environ[key] = value
-
-
-from_file(".env.template")
+@pytest.fixture(autouse=True)
+def ensure_env(monkeypatch):
+    load_dotenv(".env", override=True)
