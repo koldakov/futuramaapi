d64bcbb7e713213527bc7245f898dd98879ee878
Ivan Koldakov <ivan.koldakov@gmail.com>
2026-01-10 20:56:30 +0200

    Add API task service
    Another step to migrate from FastAPI background tasks.

--- futuramaapi/workers/services/_base.py
+++ futuramaapi/workers/services/_base.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from collections.abc import Mapping
 from typing import Any
 
+from futuramaapi.api_clients import BaseClient, HTTPXClient
 from futuramaapi.helpers.pydantic import BaseModel
 
 
@@ -9,3 +10,26 @@ class BaseTaskService[T: Mapping[str, Any]](BaseModel, ABC):
     @abstractmethod
     async def __call__(self, *args, **kwargs) -> T:
         pass
+
+
+class BaseAPITaskService[T: Mapping[str, Any]](BaseTaskService[T], ABC):
+    def __init__(
+        self,
+        *,
+        api_client: BaseClient | None = None,
+        **data: Any,
+    ) -> None:
+        super().__init__(**data)
+
+        self._api: BaseClient = api_client or HTTPXClient()
+
+    @property
+    def api(self) -> BaseClient:
+        return self._api
+
+    @abstractmethod
+    async def process(self, *args, **kwargs) -> T: ...
+
+    async def __call__(self, *args, **kwargs) -> T:
+        async with self._api:
+            return await self.process(*args, **kwargs)
