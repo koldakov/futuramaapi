2fd74b34dc60f6c9ea228b8c64b89ee3cb0ac826
Ivan Koldakov <ivan.koldakov@gmail.com>
2026-01-26 20:44:16 +0200

    Add missing get character service tests

--- tests/unit/routers/services/characters/test_get_character.py
+++ tests/unit/routers/services/characters/test_get_character.py
@@ -3,8 +3,11 @@ from unittest.mock import MagicMock
 import pytest
 from faker import Faker
 from fastapi import HTTPException
+from fastapi_storages import FileSystemStorage, StorageImage
+from pydantic import HttpUrl
 from sqlalchemy.exc import NoResultFound
 
+from futuramaapi.core import settings
 from futuramaapi.repositories import INT32
 from futuramaapi.repositories.models import CharacterModel
 from futuramaapi.routers.services.characters.get_character import GetCharacterService
@@ -26,6 +29,55 @@ class TestGetCharacterService:
         # Assert
         assert result.id == character.id
         assert result.name == character.name
+
+    @pytest.mark.asyncio
+    async def test_get_character_service_success_image_is_none(self, character: CharacterModel, mock_session_manager):
+        # Arrange
+        character.image = None
+
+        mock_result = MagicMock()
+        mock_result.scalars.return_value.one.return_value = character
+        mock_session_manager.execute.return_value = mock_result
+
+        service = GetCharacterService(pk=character.id)
+
+        # Act
+        result = await service()
+
+        # Assert
+        assert result.id == character.id
+        assert result.name == character.name
+        assert result.image is None
+
+    @pytest.mark.asyncio
+    async def test_get_character_service_success_image_is_storage_image(
+        self,
+        faker: Faker,
+        character: CharacterModel,
+        mock_session_manager,
+    ):
+        # Arrange
+        image_name: str = faker.word()
+        character.image = StorageImage(
+            name=image_name,
+            storage=FileSystemStorage(path=settings.project_root / settings.static),
+            width=faker.random_int(),
+            height=faker.random_int(),
+        )
+
+        mock_result = MagicMock()
+        mock_result.scalars.return_value.one.return_value = character
+        mock_session_manager.execute.return_value = mock_result
+
+        service = GetCharacterService(pk=character.id)
+
+        # Act
+        result = await service()
+
+        # Assert
+        assert result.id == character.id
+        assert result.name == character.name
+        assert result.image == HttpUrl(f"https://localhost/static/{image_name}")
 
     @pytest.mark.asyncio
     async def test_get_character_service_not_found(self, faker: Faker, mock_session_manager):
